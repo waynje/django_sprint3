c6a71f9f307a75d6566ff8674a276b1673b091e0
Юрий Агеев <ageev.y@yandex.ru>
2023-07-18 12:35:01 +0300

    Add first version

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,21 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Category
 from django.utils import timezone
 
-now = timezone.now
-
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id,
                              is_published=True,
                              category__is_published=True,
-                             )
+                             pub_date__lte=timezone.now())
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
 
 
 def index(request):
     posts = Post.objects.all().filter(is_published=True,
-                                      category__is_published=True).order_by(
+                                      category__is_published=True,
+                                      pub_date__lte=timezone.now()).order_by(
         '-pub_date')[:5]
     context = {'post_list': posts}
     return render(request, 'blog/index.html', context)
@@ -26,8 +25,9 @@ def category_posts(request, slug):
     category = get_object_or_404(Category,
                                  slug=slug,
                                  is_published=True)
-    posts = Post.objects.all().filter(is_published=True,
-                                      category__is_published=True,
-                                      category=category)
+    posts = Post.objects.filter(is_published=True,
+                                category__is_published=True,
+                                category=category,
+                                pub_date__lte=timezone.now())
     context = {'category': category, 'post_list': posts}
     return render(request, 'blog/category.html', context)
