88335f09ade0585465650f6ea593050b3eae34b2
Юрий Агеев <ageev.y@yandex.ru>
2023-07-21 14:38:36 +0300

    Fix third review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,32 +4,33 @@ from django.utils import timezone
 from blog.models import Post, Category
 
 
-def posts_filtered():
-    return Post.objects.filter(
-        pub_date__lte=timezone.now(),
+def posts_filtered(posts):
+    return posts.filter(
         is_published=True,
-        category__is_published=True
+        category__is_published=True,
+        pub_date__lte=timezone.now()
     )
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(posts_filtered(),
+    return render(request, 'blog/detail.html', {'post': get_object_or_404(
+                             posts_filtered(Post.objects),
                              id=post_id,
-                             )
-    context = {'post': post}
-    return render(request, 'blog/detail.html', context)
+                             )})
 
 
 def index(request):
-    posts = posts_filtered().order_by('-pub_date')[:5]
-    context = {'post_list': posts}
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/index.html',
+                  {'post_list': posts_filtered(Post.objects).order_by
+                   ('-pub_date')[:5]}
+                  )
 
 
 def category_posts(request, slug):
     category = get_object_or_404(Category,
                                  slug=slug,
                                  is_published=True)
-    posts = posts_filtered().filter(category=category)
-    context = {'category': category, 'post_list': posts}
-    return render(request, 'blog/category.html', context)
+    posts = posts_filtered(Post.objects).filter(category=category)
+    return render(request, 'blog/category.html',
+                  {'category': category,
+                   'post_list': posts})
